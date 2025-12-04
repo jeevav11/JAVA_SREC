14f1b729f536886dd001b8cb0eb194448b6ee275
Jeeva V <jeeva.2202110@srec.ac.in>
2025-12-04 12:13:48 +0530

    Create Print String Without Semicolon.java

---
+++ Basic java programs/Print String Without Semicolon.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     String s=sc.nextLine();
+     
+    for(int i=0;i<1;System.out.println(s)){
+        i++;
+    }
+  }
+}
